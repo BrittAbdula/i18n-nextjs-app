c81d38905b32b511b72ff2f63ccf48ce7e460ee7
Abdul Suri <131852838+BrittAbdula@users.noreply.github.com>
2024-03-24 23:38:20 +0800

    Create EmojiCombosList.tsx

---
+++ src/components/emojiCombosList/EmojiCombosList.tsx
@@ -0,0 +1,24 @@
+import { fetchEmojiCombos } from "@/_lib/data-emojicombo";
+import { EmojiCombo } from "@prisma/client";
+import Link from "next/link";
+
+const EmojiCombosList = async ( { query, }: { query: string} ) => {
+    const emojiCombos = await fetchEmojiCombos(query);
+    return (
+            <div className="mx-auto max-w-7xl px-5 py-4 md:px-10 md:py-4 lg:py-4">
+                <div className="grid gap-5 sm:grid-cols-2 md:grid-cols-3">
+                    {emojiCombos.map((emojiCombo: EmojiCombo) => (
+                        <div key={emojiCombo.id} className="grid gap-8 border border-solid border-[#dfdfdf] p-8 md:p-10">
+                        <p className="text-xl font-semibold ">{emojiCombo.emojis}</p>
+                            <Link href={`/emoji-combo/${emojiCombo.comboURL}`}>
+                                <p className="text-sm text-[#1A6292]">{emojiCombo.comboText}</p>
+                            </Link>
+                        </div>
+                    )
+                    )}
+                </div>
+            </div>
+    )
+}
+
+export default EmojiCombosList;
