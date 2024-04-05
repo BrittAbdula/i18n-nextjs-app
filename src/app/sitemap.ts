768c6b11fb5994b98821884839949fac3e349bdc
auroroa <auroroa@gmail.com>
2024-04-05 22:48:31 +0800

    update sitemap set  https://www.emojitell.com/

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "/",
+            url: "https://www.emojitell.com",
             lastModified: new Date().toISOString(),
             changeFrequency: "daily",
             priority: 0.7,
         },
         {
-            url: "/about",
+            url: "https://www.emojitell.com/about",
             lastModified: new Date().toISOString(),
             changeFrequency: "monthly",
             priority: 0.5,
         },
         {
-            url: "/discovery",
+            url: "https://www.emojitell.com/discovery",
             lastModified: new Date().toISOString(),
             changeFrequency: "daily",
             priority: 1,
         },
         {
-            url: "/emojis",
+            url: "https://www.emojitell.com/emojis",
             lastModified: new Date().toISOString(),
             changeFrequency: "weekly",
         }
